ab1aae4c03f87ad16cf83052ac5d148e5f1b91b2
cristian.jora <cristian.jora@corebuild.eu>
2017-09-27 15:46:59 +0300

    Formatting fixes

--- src/Grid.vue
+++ src/Grid.vue
@@ -19,198 +19,198 @@
             :item="v.item"
             :index="v.index"
             :sort="v.sort"
-            :remove="() => { removeItem(v) }"/>
+            :remove="() => { removeItem(v) }">
       </slot>
     </GridItem>
   </div>
 </template>
 <script>
-    import windowSize from './mixins/window_size.js'
-    import GridItem from './GridItem.vue'
-    export default {
-        name: 'Grid',
-        mixins: [windowSize],
-        components: {
-            GridItem
+import windowSize from './mixins/window_size.js'
+import GridItem from './GridItem.vue'
+export default {
+    name: 'Grid',
+    mixins: [windowSize],
+    components: {
+        GridItem
+    },
+    props: {
+        items: {
+            type: Array,
+            default: () => []
         },
-        props: {
-            items: {
-                type: Array,
-                default: () => []
+        gridWidth: {
+            type: Number,
+            default: -1
+        },
+        cellWidth: {
+            type: Number,
+            default: 80,
+        },
+        cellHeight: {
+            type: Number,
+            default: 80
+        },
+        draggable: {
+            type: Boolean,
+            default: false
+        },
+        dragDelay: {
+            type: Number,
+            default: 0
+        },
+        sortable: {
+            type: Boolean,
+            default: false
+        },
+        center: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {
+            list: []
+        }
+    },
+    watch: {
+        items: {
+            handler: function (nextItems = []) {
+                this.list = nextItems.map((item, index) => {
+                    return {
+                        item,
+                        index: index,
+                        sort: index
+                    }
+                })
             },
-            gridWidth: {
-                type: Number,
-                default: -1
-            },
-            cellWidth: {
-                type: Number,
-                default: 80,
-            },
-            cellHeight: {
-                type: Number,
-                default: 80
-            },
-            draggable: {
-                type: Boolean,
-                default: false
-            },
-            dragDelay: {
-                type: Number,
-                default: 0
-            },
-            sortable: {
-                type: Boolean,
-                default: false
-            },
-            center: {
-                type: Boolean,
-                default: false
+            immediate: true
+        }
+    },
+    computed: {
+        gridResponsiveWidth () {
+            if (this.gridWidth < 0) {
+                return this.windowWidth
+            } else {
+                return Math.min(this.windowWidth, this.gridWidth)
             }
         },
-        data () {
+        height () {
+            return Math.ceil(this.items.length / this.rowCount) *
+                this.cellHeight
+        },
+        style () {
             return {
-                list: []
+                height: this.height + 'px'
             }
         },
-        watch: {
-            items: {
-                handler: function (nextItems = []) {
-                    this.list = nextItems.map((item, index) => {
+        rowCount () {
+            return Math.floor(this.windowWidth / this.cellWidth)
+        },
+        rowShift () {
+            if (this.center) {
+                let contentWidth = this.items.length * this.cellWidth
+                let rowShift = contentWidth < this.windowWidth
+                    ? (this.windowWidth - contentWidth) / 2
+                    : (this.windowWidth % this.cellWidth) / 2
+                return Math.floor(rowShift)
+            }
+            return 0
+        }
+    },
+    methods: {
+        /* Returns merged event object */
+        wrapEvent (other = {}) {
+            return {
+                datetime: Date.now(),
+                items: this.getListClone(),
+                ...other
+            }
+        },
+        /* Returns sorted clone of "list" array */
+        getListClone () {
+            return this.list
+                .slice(0)
+                .sort((a, b) => {
+                    return a.sort - b.sort
+                })
+            //  .map(v => {
+            //    return { ...v.item }
+            //  })
+        },
+        removeItem ({ index }) {
+            let removeItem = this.list.find(v => v.index === index)
+            let removeItemSort = removeItem.sort
+            this.list = this.list
+                .filter(v => {
+                    return v.index !== index
+                })
+                .map(v => {
+                    let sort = v.sort > removeItemSort
+                        ? (v.sort - 1)
+                        : v.sort
+                    return { ...v, sort }
+                })
+            this.$emit('remove', this.wrapEvent({ index }))
+        },
+        onDragStart (event) {
+            this.$emit('dragstart', this.wrapEvent(event))
+        },
+        onDragEnd (event) {
+            this.$emit('dragend', this.wrapEvent(event))
+        },
+        click (event) {
+            this.$emit('click', this.wrapEvent(event))
+        },
+        onDrag (event) {
+            if (this.sortable) {
+                this.sortList(event.index, event.gridPosition)
+            }
+            this.$emit('drag', this.wrapEvent({ event }))
+        },
+        sortList (itemIndex, gridPosition) {
+            let targetItem = this.list.find(item => item.index === itemIndex)
+            let targetItemSort = targetItem.sort
+            /*
+              Normalizing new grid position
+            */
+            gridPosition = Math.max(gridPosition, 0)
+            /*
+              If you remove this line you can drag items to positions that
+              are further than items array length
+            */
+            gridPosition = Math.min(gridPosition, this.list.length - 1)
+            if (targetItemSort !== gridPosition) {
+                this.list = this.list.map(item => {
+                    if (item.index === targetItem.index) {
                         return {
-                            item,
-                            index: index,
-                            sort: index
+                            ...item,
+                            sort: gridPosition
                         }
-                    })
-                },
-                immediate: true
-            }
-        },
-        computed: {
-            gridResponsiveWidth () {
-                if (this.gridWidth < 0) {
-                    return this.windowWidth
-                } else {
-                    return Math.min(this.windowWidth, this.gridWidth)
-                }
-            },
-            height () {
-                return Math.ceil(this.items.length / this.rowCount) *
-                    this.cellHeight
-            },
-            style () {
-                return {
-                    height: this.height + 'px'
-                }
-            },
-            rowCount () {
-                return Math.floor(this.windowWidth / this.cellWidth)
-            },
-            rowShift () {
-                if (this.center) {
-                    let contentWidth = this.items.length * this.cellWidth
-                    let rowShift = contentWidth < this.windowWidth
-                        ? (this.windowWidth - contentWidth) / 2
-                        : (this.windowWidth % this.cellWidth) / 2
-                    return Math.floor(rowShift)
-                }
-                return 0
-            }
-        },
-        methods: {
-            /* Returns merged event object */
-            wrapEvent (other = {}) {
-                return {
-                    datetime: Date.now(),
-                    items: this.getListClone(),
-                    ...other
-                }
-            },
-            /* Returns sorted clone of "list" array */
-            getListClone () {
-                return this.list
-                    .slice(0)
-                    .sort((a, b) => {
-                        return a.sort - b.sort
-                    })
-                //  .map(v => {
-                //    return { ...v.item }
-                //  })
-            },
-            removeItem ({ index }) {
-                let removeItem = this.list.find(v => v.index === index)
-                let removeItemSort = removeItem.sort
-                this.list = this.list
-                    .filter(v => {
-                        return v.index !== index
-                    })
-                    .map(v => {
-                        let sort = v.sort > removeItemSort
-                            ? (v.sort - 1)
-                            : v.sort
-                        return { ...v, sort }
-                    })
-                this.$emit('remove', this.wrapEvent({ index }))
-            },
-            onDragStart (event) {
-                this.$emit('dragstart', this.wrapEvent(event))
-            },
-            onDragEnd (event) {
-                this.$emit('dragend', this.wrapEvent(event))
-            },
-            click (event) {
-                this.$emit('click', this.wrapEvent(event))
-            },
-            onDrag (event) {
-                if (this.sortable) {
-                    this.sortList(event.index, event.gridPosition)
-                }
-                this.$emit('drag', this.wrapEvent({ event }))
-            },
-            sortList (itemIndex, gridPosition) {
-                let targetItem = this.list.find(item => item.index === itemIndex)
-                let targetItemSort = targetItem.sort
-                /*
-                  Normalizing new grid position
-                */
-                gridPosition = Math.max(gridPosition, 0)
-                /*
-                  If you remove this line you can drag items to positions that
-                  are further than items array length
-                */
-                gridPosition = Math.min(gridPosition, this.list.length - 1)
-                if (targetItemSort !== gridPosition) {
-                    this.list = this.list.map(item => {
-                        if (item.index === targetItem.index) {
+                    }
+                    const { sort } = item
+                    if (targetItemSort > gridPosition) {
+                        if (sort <= targetItemSort && sort >= gridPosition) {
                             return {
                                 ...item,
-                                sort: gridPosition
+                                sort: sort + 1
                             }
                         }
-                        const { sort } = item
-                        if (targetItemSort > gridPosition) {
-                            if (sort <= targetItemSort && sort >= gridPosition) {
-                                return {
-                                    ...item,
-                                    sort: sort + 1
-                                }
+                    }
+                    if (targetItemSort < gridPosition) {
+                        if (sort >= targetItemSort && sort <= gridPosition) {
+                            return {
+                                ...item,
+                                sort: sort - 1
                             }
                         }
-                        if (targetItemSort < gridPosition) {
-                            if (sort >= targetItemSort && sort <= gridPosition) {
-                                return {
-                                    ...item,
-                                    sort: sort - 1
-                                }
-                            }
-                        }
-                        return item
-                    })
-                    this.$emit('sort', this.wrapEvent())
-                }
+                    }
+                    return item
+                })
+                this.$emit('sort', this.wrapEvent())
             }
         }
     }
+}
 </script>
 <style>
   .v-grid {
--- src/GridItem.vue
+++ src/GridItem.vue
@@ -9,172 +9,172 @@
 </template>
 
 <script>
-    const CLICK_PIXEL_DISTANCE = 4
-    export default {
-        name: 'GridItem',
-        props: {
-            index: {
-                type: Number
-            },
-            sort: {
-                type: Number
-            },
-            cellWidth: {
-                type: Number
-            },
-            cellHeight: {
-                type: Number
-            },
-            rowCount: {
-                type: Number
-            },
-            rowShift: {
-                type: Number,
-                default: 0
-            },
-            draggable: {
-                type: Boolean
-            },
-            dragDelay: {
-                type: Number,
-                default: 0
-            }
+const CLICK_PIXEL_DISTANCE = 4
+export default {
+    name: 'GridItem',
+    props: {
+        index: {
+            type: Number
         },
-        data () {
+        sort: {
+            type: Number
+        },
+        cellWidth: {
+            type: Number
+        },
+        cellHeight: {
+            type: Number
+        },
+        rowCount: {
+            type: Number
+        },
+        rowShift: {
+            type: Number,
+            default: 0
+        },
+        draggable: {
+            type: Boolean
+        },
+        dragDelay: {
+            type: Number,
+            default: 0
+        }
+    },
+    data () {
+        return {
+            animate: true,
+            dragging: false,
+            shiftStartX: 0,
+            shiftStartY: 0,
+            mouseMoveStartX: 0,
+            mouseMoveStartY: 0,
+            shiftX: 0,
+            shiftY: 0,
+            timer: null,
+            zIndex: 1
+        }
+    },
+    mounted () {
+        this.$refs.self
+            .addEventListener('transitionend', (event) => {
+                if (!this.dragging) {
+                    this.zIndex = 1
+                }
+            }, false)
+    },
+    computed: {
+        className () {
+            let { animate, dragging } = this
+            return [
+                'v-grid-item-wrapper',
+                {
+                    'v-grid-item-animate': animate,
+                    'v-grid-item-dragging': dragging
+                }
+            ]
+        },
+        style () {
+            let { zIndex, cellWidth, cellHeight, top, left } = this
             return {
-                animate: true,
-                dragging: false,
-                shiftStartX: 0,
-                shiftStartY: 0,
-                mouseMoveStartX: 0,
-                mouseMoveStartY: 0,
-                shiftX: 0,
-                shiftY: 0,
-                timer: null,
-                zIndex: 1
+                zIndex,
+                width: cellWidth + 'px',
+                height: cellHeight + 'px',
+                transform: `translate3d(${left}px, ${top}px, 0)`
             }
         },
-        mounted () {
-            this.$refs.self
-                .addEventListener('transitionend', (event) => {
-                    if (!this.dragging) {
-                        this.zIndex = 1
-                    }
-                }, false)
+        left () {
+            return this.dragging
+                ? this.shiftX
+                : this.rowShift + (this.sort % this.rowCount) * this.cellWidth
         },
-        computed: {
-            className () {
-                let { animate, dragging } = this
-                return [
-                    'v-grid-item-wrapper',
-                    {
-                        'v-grid-item-animate': animate,
-                        'v-grid-item-dragging': dragging
-                    }
-                ]
-            },
-            style () {
-                let { zIndex, cellWidth, cellHeight, top, left } = this
-                return {
-                    zIndex,
-                    width: cellWidth + 'px',
-                    height: cellHeight + 'px',
-                    transform: `translate3d(${left}px, ${top}px, 0)`
-                }
-            },
-            left () {
-                return this.dragging
-                    ? this.shiftX
-                    : this.rowShift + (this.sort % this.rowCount) * this.cellWidth
-            },
-            top () {
-                return this.dragging
-                    ? this.shiftY
-                    : Math.floor(this.sort / this.rowCount) * this.cellHeight
+        top () {
+            return this.dragging
+                ? this.shiftY
+                : Math.floor(this.sort / this.rowCount) * this.cellHeight
+        }
+    },
+    methods: {
+        wrapEvent (event) {
+            let { index, sort } = this
+            return { event, index, sort }
+        },
+        dragStart (event) {
+            let e = event.touches ? event.touches[0] : event
+            this.zIndex = 2
+            this.shiftX = this.shiftStartX = this.left
+            this.shiftY = this.shiftStartY = this.top
+            this.mouseMoveStartX = e.pageX
+            this.mouseMoveStartY = e.pageY
+            this.animate = false
+            this.dragging = true
+            document.addEventListener('mousemove', this.documentMouseMove)
+            document.addEventListener('touchmove', this.documentMouseMove)
+            this.$emit('dragstart', this.wrapEvent(event))
+        },
+        drag (event) {
+            let e = event.touches ? event.touches[0] : event
+            let distanceX = e.pageX - this.mouseMoveStartX
+            let distanceY = e.pageY - this.mouseMoveStartY
+            this.shiftX = distanceX + this.shiftStartX
+            this.shiftY = distanceY + this.shiftStartY
+            let gridX = Math.round(this.shiftX / this.cellWidth)
+            let gridY = Math.round(this.shiftY / this.cellHeight)
+            gridX = Math.min(gridX, this.rowCount - 1)
+            gridY = Math.max(gridY, 0)
+            let gridPosition = gridX + gridY * this.rowCount
+            const $event = {
+                event,
+                distanceX,
+                distanceY,
+                positionX: this.shiftX,
+                positionY: this.shiftY,
+                index: this.index,
+                gridX,
+                gridY,
+                gridPosition
+            }
+            this.$emit('drag', $event)
+        },
+        mousedown (event) {
+            if (this.draggable) {
+                this.timer = setTimeout(() => {
+                    this.dragStart(event)
+                }, this.dragDelay)
+                document.addEventListener('mouseup', this.documentMouseUp)
+                document.addEventListener('touchend', this.documentMouseUp)
             }
         },
-        methods: {
-            wrapEvent (event) {
-                let { index, sort } = this
-                return { event, index, sort }
-            },
-            dragStart (event) {
-                let e = event.touches ? event.touches[0] : event
-                this.zIndex = 2
-                this.shiftX = this.shiftStartX = this.left
-                this.shiftY = this.shiftStartY = this.top
-                this.mouseMoveStartX = e.pageX
-                this.mouseMoveStartY = e.pageY
-                this.animate = false
-                this.dragging = true
-                document.addEventListener('mousemove', this.documentMouseMove)
-                document.addEventListener('touchmove', this.documentMouseMove)
-                this.$emit('dragstart', this.wrapEvent(event))
-            },
-            drag (event) {
-                let e = event.touches ? event.touches[0] : event
-                let distanceX = e.pageX - this.mouseMoveStartX
-                let distanceY = e.pageY - this.mouseMoveStartY
-                this.shiftX = distanceX + this.shiftStartX
-                this.shiftY = distanceY + this.shiftStartY
-                let gridX = Math.round(this.shiftX / this.cellWidth)
-                let gridY = Math.round(this.shiftY / this.cellHeight)
-                gridX = Math.min(gridX, this.rowCount - 1)
-                gridY = Math.max(gridY, 0)
-                let gridPosition = gridX + gridY * this.rowCount
-                const $event = {
-                    event,
-                    distanceX,
-                    distanceY,
-                    positionX: this.shiftX,
-                    positionY: this.shiftY,
-                    index: this.index,
-                    gridX,
-                    gridY,
-                    gridPosition
-                }
-                this.$emit('drag', $event)
-            },
-            mousedown (event) {
-                if (this.draggable) {
-                    this.timer = setTimeout(() => {
-                        this.dragStart(event)
-                    }, this.dragDelay)
-                    document.addEventListener('mouseup', this.documentMouseUp)
-                    document.addEventListener('touchend', this.documentMouseUp)
-                }
-            },
-            documentMouseMove (event) {
-                if (this.draggable && this.dragging) {
-                    this.drag(event)
-                }
-            },
-            documentMouseUp (event) {
-                if (this.timer) {
-                    clearTimeout(this.timer)
-                    this.timer = null
-                }
-                let dx = this.shiftStartX - this.shiftX
-                let dy = this.shiftStartY - this.shiftY
-                let distance = Math.sqrt(dx * dx + dy * dy)
-                this.animate = true
-                this.dragging = false
-                this.mouseMoveStartX = 0
-                this.mouseMoveStartY = 0
-                this.shiftStartX = 0
-                this.shiftStartY = 0
-                document.removeEventListener('mousemove', this.documentMouseMove)
-                document.removeEventListener('touchmove', this.documentMouseMove)
-                document.removeEventListener('mouseup', this.documentMouseUp)
-                document.removeEventListener('touchend', this.documentMouseUp)
-                let $event = this.wrapEvent(event)
-                if (distance < CLICK_PIXEL_DISTANCE) {
-                    this.$emit('click', $event)
-                }
-                this.$emit('dragend', $event)
+        documentMouseMove (event) {
+            if (this.draggable && this.dragging) {
+                this.drag(event)
             }
+        },
+        documentMouseUp (event) {
+            if (this.timer) {
+                clearTimeout(this.timer)
+                this.timer = null
+            }
+            let dx = this.shiftStartX - this.shiftX
+            let dy = this.shiftStartY - this.shiftY
+            let distance = Math.sqrt(dx * dx + dy * dy)
+            this.animate = true
+            this.dragging = false
+            this.mouseMoveStartX = 0
+            this.mouseMoveStartY = 0
+            this.shiftStartX = 0
+            this.shiftStartY = 0
+            document.removeEventListener('mousemove', this.documentMouseMove)
+            document.removeEventListener('touchmove', this.documentMouseMove)
+            document.removeEventListener('mouseup', this.documentMouseUp)
+            document.removeEventListener('touchend', this.documentMouseUp)
+            let $event = this.wrapEvent(event)
+            if (distance < CLICK_PIXEL_DISTANCE) {
+                this.$emit('click', $event)
+            }
+            this.$emit('dragend', $event)
         }
     }
+}
 </script>
 
 <style>
